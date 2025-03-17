c5c45c8313cd48a8f1a6c8f5c20c99dee165affb
Dominik Strangas <dos@@format-d.com>
2025-03-17 16:19:03 +0100

    feat: removed stores and cleared template

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,19 +1,9 @@
 <script setup>
-import { useCounterStore } from '../stores/counter';
-import { storeToRefs } from 'pinia';
 
-const counterStore = useCounterStore();
-const { count, doubleCount } = storeToRefs(counterStore);
-
-const increment = () => counterStore.increment();
-const decrement = () => counterStore.decrement();
 </script>
 
 <template>
   <div class="">
-    <h2>Counter: {{ count }}</h2>
-    <h3>Double: {{ doubleCount }}</h3>
-    <button @click="increment">+</button>
-    <button @click="decrement">-</button>
+
   </div>
 </template>
