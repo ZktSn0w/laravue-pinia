f0356145fe6d67a7d9089bf49b25f19eeb5e0aa1
Dominik Strangas <dos@@format-d.com>
2025-03-17 16:18:04 +0100

    feat: working tailwindcss setup

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -10,7 +10,7 @@ const decrement = () => counterStore.decrement();
 </script>
 
 <template>
-  <div class="bg-red-500">
+  <div class="">
     <h2>Counter: {{ count }}</h2>
     <h3>Double: {{ doubleCount }}</h3>
     <button @click="increment">+</button>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,10 +3,10 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    @vite('resources/js/app.ts')
+    @vite(['resources/js/app.ts', 'resources/css/app.css'])
     @inertiaHead
   </head>
-  <body>
+  <body class="">
     @inertia
   </body>
 </html>
