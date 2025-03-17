7f4935fab55b99956cee4d36cbaa1c1fc4b9d1e5
Dominik Strangas <dos@@format-d.com>
2025-03-17 08:06:09 +0100

    feat: main setup

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useCounterStore } from '../stores/counter';
+import { storeToRefs } from 'pinia';
+
+const counterStore = useCounterStore();
+const { count, doubleCount } = storeToRefs(counterStore);
+
+const increment = () => counterStore.increment();
+const decrement = () => counterStore.decrement();
+</script>
+
+<template>
+  <div>
+    <h2>Counter: {{ count }}</h2>
+    <h3>Double: {{ doubleCount }}</h3>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+  </div>
+</template>
